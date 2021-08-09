277322d7cc0652560d13e7024402c9149e4e1615
HardBoris <bgaibor03@gmail.com>
2021-08-09 14:10:52 -0400

    SnapCrackle sem bonus

--- SnapCrackle.js
+++ SnapCrackle.js
@@ -1,39 +1,25 @@
 function snapCrackle(maxValue) {
+
     let cad = new Array(maxValue)
+
     for (let i = 0; i <= maxValue; i++) {
+
         if (i % 2 !== 0 && i % 5 === 0) {
             cad[i] = "SnapCrackle"
             
         } else if (i % 2 !== 0) {
             cad[i] = "Snap"
+
         } else if (i % 2 === 0 && i % 5 === 0) {
             cad[i] = "Crackle"
+
         } else {
             cad[i] = i
         }
     }
+
     cad.shift()
     return cad.toString()
 }
 
 
-function multiplos5(v) {
-    let fiv = new Array(v)
-    
-    for (let i = 0; i <= v; i++) {
-
-        if (i % 2 !== 0 && i % 5 === 0) {
-            fiv[i] = "SnapCrackle"
-            
-        } else if (i % 2 !== 0) {
-            fiv[i] = "Snap"
-        } else if (i % 2 === 0 && i % 5 === 0) {
-            fiv[i] = "Crackle"
-        } else {
-            fiv[i] = i
-        }
-    }
-    return fiv
-}
-
-console.log(multiplos5())
