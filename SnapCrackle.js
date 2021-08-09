385125e45653759feb34e0425174c840ac14070f
HardBoris <bgaibor03@gmail.com>
2021-08-09 11:14:47 -0400

    Função SnapCrackle ok

--- SnapCrackle.js
+++ SnapCrackle.js
@@ -1,11 +1,39 @@
 function snapCrackle(maxValue) {
-    let cad = ""
-    for (let i = 1; i <= maxValue; i++) {
-        cad = cad
-        cad = cad + i
-        
+    let cad = new Array(maxValue)
+    for (let i = 0; i <= maxValue; i++) {
+        if (i % 2 !== 0 && i % 5 === 0) {
+            cad[i] = "SnapCrackle"
+            
+        } else if (i % 2 !== 0) {
+            cad[i] = "Snap"
+        } else if (i % 2 === 0 && i % 5 === 0) {
+            cad[i] = "Crackle"
+        } else {
+            cad[i] = i
+        }
     }
-    return cad
+    cad.shift()
+    return cad.toString()
 }
 
-console.assert(snapCrackle(10) === "12345678910", "algo está errado")
+
+function multiplos5(v) {
+    let fiv = new Array(v)
+    
+    for (let i = 0; i <= v; i++) {
+
+        if (i % 2 !== 0 && i % 5 === 0) {
+            fiv[i] = "SnapCrackle"
+            
+        } else if (i % 2 !== 0) {
+            fiv[i] = "Snap"
+        } else if (i % 2 === 0 && i % 5 === 0) {
+            fiv[i] = "Crackle"
+        } else {
+            fiv[i] = i
+        }
+    }
+    return fiv
+}
+
+console.log(multiplos5())
