6412ddc930c4f5f8ac6206509ea2ae9c4818220f
HardBoris <bgaibor03@gmail.com>
2021-08-08 22:03:01 -0400

    un paso a la vez

--- SnapCrackle.js
+++ SnapCrackle.js
@@ -1,3 +1,11 @@
 function snapCrackle(maxValue) {
-    
+    let cad = ""
+    for (let i = 1; i <= maxValue; i++) {
+        cad = cad
+        cad = cad + i
+        
+    }
+    return cad
 }
+
+console.assert(snapCrackle(10) === "12345678910", "algo está errado")
